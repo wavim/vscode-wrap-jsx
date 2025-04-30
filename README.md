0042a0a3a06e30527a21bff7c933231dedc53620
carbonsoda <carbonsoda@outlook.com>
2025-04-30 20:15:44 +0800

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+<h3 align="center">
+	<img src="https://raw.githubusercontent.com/CarbonicSoda/vscode-wrap-jsx/master/media/icon.png" width="130" alt="VSCode Wrap JSX" /><br />
+  VSCode Wrap JSX
+</h3>
+<p align="center">Simple JSX Selection Wrapper</p>
+
+---
+
+Select target elements/components, `Ctrl+Alt+W` and... _voilà_! They're wrapped
+with tags for you to fill.
+
+This is intended to fix some other buggy extensions doing the same, making this
+more consistent and stable.
+
+Works in HTML, JSx, TSx and Vue.
---
+++ src/extension.ts
@@ -0,0 +1,38 @@
+import { commands, ExtensionContext, Selection, TextEditor } from "vscode";
+
+export function activate(context: ExtensionContext): void {
+	context.subscriptions.push(
+		commands.registerTextEditorCommand("wrap-jsx.wrap", wrap),
+	);
+}
+
+export function deactivate(): void {}
+
+async function wrap(editor: TextEditor): Promise<void> {
+	const selection = editor.selection;
+	if (selection.isEmpty) return;
+
+	const cursors: Selection[] = [];
+
+	const apply = await editor.edit((edit) => {
+		edit.insert(selection.start, "<>");
+		edit.insert(selection.end, "</>");
+
+		const insertOpenPos = selection.start.translate(0, 1);
+		const insertClosePos = selection.end.translate(0, 2);
+
+		const openCursor = new Selection(insertOpenPos, insertOpenPos);
+		const closeCursor = new Selection(insertClosePos, insertClosePos);
+
+		cursors.push(openCursor, closeCursor);
+	});
+
+	if (!apply) return;
+
+	editor.selections = cursors;
+
+	const isHtml = editor.document.languageId === "html";
+	if (!isHtml) {
+		await commands.executeCommand("editor.action.formatDocument");
+	}
+}
